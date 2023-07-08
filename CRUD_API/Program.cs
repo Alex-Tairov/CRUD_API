91efd43970343cfc1328297d2bc4a44fdcf0e647
Admin <Admin@DESKTOP-JD7A7FJ>
2023-07-08 13:25:59 +0300

    Добавил строки для подключения к БД

--- CRUD_API/Program.cs
+++ CRUD_API/Program.cs
@@ -1,3 +1,9 @@
+using CRUD_API.Database;
+using CRUD_API.Helpers;
+using CRUD_API.Interfaces;
+using CRUD_API.Storages;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("API_MySQL")));
+
+builder.Services.AddTransient<IResidentStorage, ResidentStorage>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 
